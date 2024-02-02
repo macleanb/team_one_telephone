1ae4d0eaac4913135bee9a2535b7b2718f6f8fb2
Errick Basa-Denis <errickbd@gmail.com>
2024-02-02 16:54:50 -0600

    team3 commit

---
+++ telephone.py
@@ -0,0 +1,17 @@
+#pip install beautifulsoup4
+#pip install google
+
+import webbrowser
+#open a new broswser
+
+def open_browser():
+    #make a url variable
+    url = 'https://www.google.com/?client=safari'
+    webbrowser.open(url)
+
+    inputSearch = input("What do you want to search?")
+    
+
+
+
+    # if __name__ == "__main__" :
