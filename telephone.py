9e3aff739bf7d1b8742fe2722593fa4bdc73e0fe
Errick Basa-Denis <errickbd@gmail.com>
2024-02-04 11:05:33 -0600

    Implement Internet Search

--- telephone.py
+++ telephone.py
@@ -1,17 +1,24 @@
-#pip install beautifulsoup4
-#pip install google
-
 import webbrowser
-#open a new broswser
+import json
 
-def open_browser():
-    #make a url variable
-    url = 'https://www.google.com/?client=safari'
-    webbrowser.open(url)
+def search_browser():
 
-    inputSearch = input("What do you want to search?")
-    
+    #ask for parameters and change spaces to dash
+    input_search = input("What do you want to search? ")
+    input_search_url = input_search.replace(' ', '-').lower()
+
+    #make a url variable to include search criteria
+    search_url = f'https://www.google.com/search?q={input_search_url}'
+
+    #opens the browser with search criteria
+    webbrowser.open(search_url)
+
+
+    #adds search history to a json file
+    with open('search_history.json', 'a') as search_object:
+        json.dump({'Search': input_search, 'URL': search_url} , search_object)
+        search_object.write("\n") 
 
 
 
-    # if __name__ == "__main__" :
+search_browser()
